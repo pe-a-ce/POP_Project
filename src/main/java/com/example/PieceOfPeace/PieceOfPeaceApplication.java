9564232851d9c9f0f45502268ec8a7528ffb0ec5
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-05-23 08:59:21 +0100

    initial commit - spring initialiser skeleton

---
+++ src/main/java/com/example/PieceOfPeace/PieceOfPeaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.PieceOfPeace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieceOfPeaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieceOfPeaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PieceOfPeace/PieceOfPeaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PieceOfPeace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieceOfPeaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
